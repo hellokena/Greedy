7ade2a361e87e619f7fabf95a85b32fc577e1ce8
hellokena <42730820+hellokena@users.noreply.github.com>
2021-04-09 04:31:31 +0900

    4/8

--- BOJ#1434_책 정리.py
+++ BOJ#1434_책 정리.py
@@ -9,7 +9,7 @@ book = 0 # 현재 책 번호
 while True:
     if box_size[box] >= book_size[book]: # 남아있는 용량이 있으면
         box_size[box] -= book_size[book] 
-        if box_size[box] == 0: box += 1 # 넣은 다음에 박스 크기가 0이면 다음 박스 가져옴
+        #if box_size[box] == 0: box += 1 # 넣은 다음에 박스 크기가 0이면 다음 박스 가져옴
         book += 1 # 다음 책 가져옴
     else: # 남아있는 용량이 없음
         trash += (box_size[box]) # 쓰레기 용량 더하기
